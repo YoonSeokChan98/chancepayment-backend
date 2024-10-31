d4dcbc37d4305e73685354bc0ffb17f6a0fcb4ac
Y0_0N98 <shift71895@gmail.com>
2024-10-31 19:18:55 +0900

    ㅁㄴㅇ

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: 'http://localhost:3000', // 프론트엔드의 주소
-        credentials: true, // 인증 정보를 포함할지 여부
-    })
+  cors({
+    origin: 'http://localhost:3000', // 프론트엔드의 주소
+    credentials: true, // 인증 정보를 포함할지 여부
+  })
 );
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 // 간단한 테스트 라우트
 app.get('/ping', (req, res) => {
-    res.send('pong');
+  res.send('pong');
 });
 
 // api 라우트
@@ -30,12 +30,12 @@ app.use('/api/user', userRouter);
 
 // DB 연결 및 서버 시작
 db.sequelize
-    .sync({ force: false })
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error('Unable to connect to the database:', err);
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
